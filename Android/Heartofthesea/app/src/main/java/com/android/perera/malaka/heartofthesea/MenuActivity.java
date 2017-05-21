07569d1e9ddc639e69b55b5e8ce92db3d1f88b86
MalakaP95 <malakaperera887@gmail.com>
2017-05-21 10:38:25 +1000

    Adding Android version base project of the game

---
+++ Android/Heartofthesea/app/src/main/java/com/android/perera/malaka/heartofthesea/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.android.perera.malaka.heartofthesea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
